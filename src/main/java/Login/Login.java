52b7c4d2c89eabd0959301ba6907b11b2c3a47ca
DESKTOP-4QU2PH6\Lky <93perfect@naver.com>
2023-01-17 23:51:50 +0900

    no message

--- src/main/java/Login/Login.java
+++ src/main/java/Login/Login.java
@@ -15,6 +15,7 @@ public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("회원가입");
 		doPost(request, response);
 	}
 
