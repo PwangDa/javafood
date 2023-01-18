a0bc731181d397c476b129ad027c28def935f3f5
황다영 <hdy9258@naver.com>
2023-01-18 13:37:48 +0900

    artist.jsp 작업

---
+++ src/main/java/comment/CommentDAO.java
@@ -0,0 +1,118 @@
+package comment;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class CommentDAO {
+	
+	private Connection con;
+	private PreparedStatement pstmt;
+	private DataSource dataFactory;
+	
+	//sql 불러오는 설정
+	public CommentDAO(){
+		Context ctx;
+		try {
+			ctx = new InitialContext();
+			Context envContext = (Context) ctx.lookup("java:/comp/env");
+			dataFactory = (DataSource)envContext.lookup("jdbc/javafood"); 
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//댓글 등록 구현
+	public void addcomment(CommentVO commentVO) {
+		try {
+			this.con = dataFactory.getConnection();
+			
+			String id = commentVO.getComment_id();
+			String cont = commentVO.getComment_cont();
+			
+			String query = "insert into comment_c";
+			query += "(comment_num, comment_id, comment_cont)";
+			query += " values(comm_c.nextval, ?, ?)"; //띄어쓰기 필수!
+			
+			System.out.println("query check" + query);
+			
+			pstmt = con.prepareStatement(query);
+			
+			pstmt.setString(1, id);
+			pstmt.setString(2, cont);
+			pstmt.executeUpdate();
+			
+			pstmt.close();
+			con.close();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//댓글 리스트 읽기 구현
+	public List<CommentVO> listComment(){
+		List<CommentVO> list = new ArrayList<CommentVO>();
+		
+		try {
+			this.con = dataFactory.getConnection();
+			
+			   //가져올 테이블 선택(불러오기)
+			   String query = "select * from comment_c";
+			   System.out.println(query);
+			   
+			   pstmt = this.con.prepareStatement(query);
+			   ResultSet rs = pstmt.executeQuery();
+			   
+			   while(rs.next()) {
+				   String id = rs.getString("comment_id");
+				   String cont = rs.getString("comment_cont");
+				   Date date = rs.getDate("comment_date");
+				   
+				   CommentVO vo = new CommentVO();
+				   vo.setComment_id(id);
+				   vo.setComment_cont(cont);
+				   vo.setComment_Date(date);
+				   list.add(vo);
+			   }
+			   
+			   rs.close();
+			   pstmt.close();
+			   con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+	
+	//댓글삭제버튼 구현
+	public void delcomment(String id) {
+		try {
+			this.con = dataFactory.getConnection();
+			
+			String query = " delete from comment_t";
+			query +=       " where comment_id = ?";
+			
+			pstmt = con.prepareStatement(query);
+			pstmt.setString(1, id);
+			
+			int result = pstmt.executeUpdate();
+			System.out.println("삭제결과 확인"+result);
+			pstmt.close();
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+}
--- src/main/java/comment/CommentVO.java
+++ src/main/java/comment/CommentVO.java
@@ -1,8 +1,37 @@
 package comment;
 
-public class CommentVO {
-	private void sysout() {
-		System.out.println("다영작업실");
+import java.sql.Date;
 
+public class CommentVO {
+	
+	private String comment_num;
+	private String comment_id;
+	private String comment_cont;
+	private Date comment_Date;
+	
+	public String getComment_num() {
+		return comment_num;
 	}
+	public void setComment_num(String comment_num) {
+		this.comment_num = comment_num;
+	}
+	public String getComment_id() {
+		return comment_id;
+	}
+	public void setComment_id(String comment_id) {
+		this.comment_id = comment_id;
+	}
+	public String getComment_cont() {
+		return comment_cont;
+	}
+	public void setComment_cont(String comment_cont) {
+		this.comment_cont = comment_cont;
+	}
+	public Date getComment_Date() {
+		return comment_Date;
+	}
+	public void setComment_Date(Date comment_Date) {
+		this.comment_Date = comment_Date;
+	}
+
 }
