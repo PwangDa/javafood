c058f0a323203e9b6a754c3402ffea9a8c6d789a
이용준 <dldydwns0710@naver.com>
2023-01-19 11:38:18 +0900

    dbon수정

--- src/main/java/My_Page/dbon.java
+++ src/main/java/My_Page/dbon.java
@@ -53,15 +53,16 @@ public class dbon {
 		return st ;
 	}
 	
-	public List<vod> ballade () {
+	public List<vod> getGenre (String a) {
 		List<vod> list = new ArrayList<>();
 		
 		
 		try {
 			con = dataFactory.getConnection();
 			String genre = " SELECT * FROM  song";
-			genre += " where bygenre = '발라드'";
+			genre += " where bygenre = ?";
 			pstmt = con.prepareStatement (genre);
+			pstmt.setString(1, a);
 			ResultSet rs = pstmt.executeQuery();
 			while(rs.next()) {
 				vod vo = new vod();
@@ -79,144 +80,5 @@ public class dbon {
 		}
 		return list;
 	}
-	
-	public List<vod> dance () {
-		List<vod> list = new ArrayList<>();
-		
-		
-		try {
-			con = dataFactory.getConnection();
-			String genre = " SELECT * FROM  song";
-			genre += " where bygenre = '댄스'";
-			pstmt = con.prepareStatement (genre);
-			ResultSet rs = pstmt.executeQuery();
-			while(rs.next()) {
-				vod vo = new vod();
-				vo.setArtistname(rs.getString("artistname"));
-				vo.setBygenre(rs.getString("bygenre"));
-				vo.setHits(rs.getString("hits"));
-				vo.setLikes(rs.getString("likes"));
-				vo.setSongname(rs.getString("songname"));
-				vo.setSongnumber(rs.getString("songnumber"));
-				vo.setLink(rs.getString("link"));
-				list.add(vo);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return list;
-	}
-	
-	public List<vod> pop () {
-		List<vod> list = new ArrayList<>();
-		
-		
-		try {
-			con = dataFactory.getConnection();
-			String genre = " SELECT * FROM  song";
-			genre += " where bygenre = 'pop'";
-			pstmt = con.prepareStatement (genre);
-			ResultSet rs = pstmt.executeQuery();
-			while(rs.next()) {
-				vod vo = new vod();
-				vo.setArtistname(rs.getString("artistname"));
-				vo.setBygenre(rs.getString("bygenre"));
-				vo.setHits(rs.getString("hits"));
-				vo.setLikes(rs.getString("likes"));
-				vo.setSongname(rs.getString("songname"));
-				vo.setSongnumber(rs.getString("songnumber"));
-				vo.setLink(rs.getString("link"));
-				list.add(vo);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return list;
-	}
-	
-	
-	public List<vod> rnb () {
-		List<vod> list = new ArrayList<>();
-		
-		
-		try {
-			con = dataFactory.getConnection();
-			String genre = " SELECT * FROM  song";
-			genre += " where bygenre = 'R&B'";
-			pstmt = con.prepareStatement (genre);
-			ResultSet rs = pstmt.executeQuery();
-			while(rs.next()) {
-				vod vo = new vod();
-				vo.setArtistname(rs.getString("artistname"));
-				vo.setBygenre(rs.getString("bygenre"));
-				vo.setHits(rs.getString("hits"));
-				vo.setLikes(rs.getString("likes"));
-				vo.setSongname(rs.getString("songname"));
-				vo.setSongnumber(rs.getString("songnumber"));
-				vo.setLink(rs.getString("link"));
-				list.add(vo);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return list;
-	}
-	
-	
-	public List<vod> indy () {
-		List<vod> list = new ArrayList<>();
-		
-		
-		try {
-			con = dataFactory.getConnection();
-			String genre = " SELECT * FROM  song";
-			genre += " where bygenre = '인디'";
-			pstmt = con.prepareStatement (genre);
-			ResultSet rs = pstmt.executeQuery();
-			while(rs.next()) {
-				vod vo = new vod();
-				vo.setArtistname(rs.getString("artistname"));
-				vo.setBygenre(rs.getString("bygenre"));
-				vo.setHits(rs.getString("hits"));
-				vo.setLikes(rs.getString("likes"));
-				vo.setSongname(rs.getString("songname"));
-				vo.setSongnumber(rs.getString("songnumber"));
-				vo.setLink(rs.getString("link"));
-				list.add(vo);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return list;
-	}
-	
-	
-	public List<vod> trot () {
-		List<vod> list = new ArrayList<>();
-		
-		
-		try {
-			con = dataFactory.getConnection();
-			String genre = " SELECT * FROM  song";
-			genre += " where bygenre = '트로트'";
-			pstmt = con.prepareStatement (genre);
-			ResultSet rs = pstmt.executeQuery();
-			while(rs.next()) {
-				vod vo = new vod();
-				vo.setArtistname(rs.getString("artistname"));
-				vo.setBygenre(rs.getString("bygenre"));
-				vo.setHits(rs.getString("hits"));
-				vo.setLikes(rs.getString("likes"));
-				vo.setSongname(rs.getString("songname"));
-				vo.setSongnumber(rs.getString("songnumber"));
-				vo.setLink(rs.getString("link"));
-				list.add(vo);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return list;
-	}
-	
 	
 }
