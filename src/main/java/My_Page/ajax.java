853e1299f858f4878a29bf6e7e40d7a1f2f6a390
이경용 <93perfect@naver.com>
2023-01-27 16:23:07 +0900

    ajax

--- src/main/java/My_Page/ajax.java
+++ src/main/java/My_Page/ajax.java
@@ -16,43 +16,47 @@ public class ajax extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String id = request.getParameter("id");
-		String pw = request.getParameter("pw");
 		String nic = request.getParameter("nic");
 		String email = request.getParameter("email");
 		String pn = request.getParameter("pn");
 		String phone = request.getParameter("phone");
 		dbon db = new dbon();
-		int a1 = 0;
-		int a2 = 0;
-		int a3 = 0;
-		int a4 = 0;
-		int a5 = 0;
-		int a6 = 0;
+		int a = 0;
 		
 		List<vod> vo = db.listID();
 		for(int i=0; i<vo.size();i++) {
-			if(id.equals(vo.get(i).getId())) {
-				System.out.println(vo.get(i).getId());
-				a1=1;
+			if(id!=null) {
+				if(id.equals(vo.get(i).getId())) {
+					System.out.println(vo.get(i).getId());
+					a=1;
+				}
 			}
-			if(id.equals(vo.get(i).getNic())) {
-				System.out.println(vo.get(i).getNic());
-				a1=1;
+			if(nic!=null) {
+				if(nic.equals(vo.get(i).getNic())) {
+					System.out.println(vo.get(i).getNic());
+					a=1;
+				}
 			}
-			if(id.equals(vo.get(i).getEmail())) {
-				System.out.println(vo.get(i).getEmail());
-				a1=1;
+			if(email!=null) {
+				if(email.equals(vo.get(i).getEmail())) {
+					System.out.println(vo.get(i).getEmail());
+					a=1;
+				}
 			}
-			if(id.equals(vo.get(i).getPn())) {
-				System.out.println(vo.get(i).getPn());
-				a1=1;
+			if(pn!=null) {
+				if(pn.equals(vo.get(i).getPn())) {
+					System.out.println(vo.get(i).getPn());
+					a=1;
+				}
 			}
-			if(id.equals(vo.get(i).getPhone())) {
-				System.out.println(vo.get(i).getPhone());
-				a1=1;
+			if(phone!=null) {
+				if(phone.equals(vo.get(i).getPhone())) {
+					System.out.println(vo.get(i).getPhone());
+					a=1;
+				}
 			}
 		}
-		response.getWriter().print(a1);
+		response.getWriter().print(a);
 		
 	}
 }
