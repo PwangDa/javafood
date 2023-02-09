ef31b565d47040c7d1d96bd1adbf0753f3dbac67
최귀범 <cgb416@naver.com>
2023-02-10 08:47:50 +0900

    Song.jsp 수정작업중입니다

--- src/main/java/SecondProject/JavaFood_Service.java
+++ src/main/java/SecondProject/JavaFood_Service.java
@@ -5,27 +5,14 @@ import java.util.List;
 import Chart.SongDAO;
 import My_Page.vod;
 
+
+
 public class JavaFood_Service {
-	
-	JavaFood_DAO dao;
-	
-	JavaFood_Service(){
-		dao = new JavaFood_DAO();
-	}
-	
-	
-//	다영의 메소드(앨범목록) : 아직 vo 패키지에 안넣었음
-//    public List<AlbumVO> Albumlist(){
-//    	List<AlbumVO> Albumlist = javafoodDAO.listAlbum(); 
-//    	return Albumlist; 
-//    }
-	
+
 	public void javafood1(){
 		
 	}
 	public void javafood2(){
-		
-		SongDAO songDAO = new SongDAO();
 		
 	}
 	public void javafood3(){
