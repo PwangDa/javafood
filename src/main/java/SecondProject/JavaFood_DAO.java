13fffe0ffb6796f16ad4852ad2705946f5f3bc36
kimtaeyeon <aaa123@naver.com>
2023-02-15 15:26:00 +0900

    ..

--- src/main/java/SecondProject/JavaFood_DAO.java
+++ src/main/java/SecondProject/JavaFood_DAO.java
@@ -1169,9 +1169,41 @@ public class JavaFood_DAO {
 		
 		return playListContent;
 	}
-}
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
+	public void addd(String a, int b) {
+		List li = new ArrayList();
+		try {
+			this.con = this.dataFactory.getConnection();
+			this.con.prepareStatement("UPDATE GENRE SET album_add='"+a+"' WHERE SONGNUMBER ='"+b+"'").executeUpdate();
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		public List<AlbumDTO> addd1() {
+			List<AlbumDTO> li = new ArrayList<AlbumDTO>();
+			try {
+				this.con = this.dataFactory.getConnection();
+				ResultSet rs = this.con.prepareStatement("SELECT album_add FROM GENRE").executeQuery();
+				while(rs.next()) {
+					
+					String n = rs.getString("album_add");
+					AlbumDTO dto = new AlbumDTO();
+					dto.setName(n);
+					li.add(dto);
+				}
+				
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			return li;
+		}
+		
+		
+		
+		
+		
+	}
 
