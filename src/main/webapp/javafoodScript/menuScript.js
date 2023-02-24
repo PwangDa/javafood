65d3ef899158d6039453e623606466d67b2dcee8
이경용 <93perfect@naver.com>
2023-02-24 11:39:29 +0900

    매뉴완성

--- src/main/webapp/javafoodScript/menuScript.js
+++ src/main/webapp/javafoodScript/menuScript.js
@@ -13,3 +13,10 @@ $('#spa').on('mouseover',function(){
 $('#di').on('mouseleave',function(){
 	$('#di').slideUp();
 })
+function outt(id){
+	if (confirm('정말 로그아웃을 하겠습니까?')) 
+		location.href = 'javafood?javafood=5&iid='+id
+    else 
+    	alert('취소하였습니다.');
+    }
+
