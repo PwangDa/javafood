3e64203ba24c4d803a073a2eddcc635b0bef360f
이경용 <93perfect@naver.com>
2023-02-24 10:02:16 +0900

    매뉴 태그

--- src/main/webapp/javafoodScript/menuScript.js
+++ src/main/webapp/javafoodScript/menuScript.js
@@ -1,5 +1,15 @@
+$("#di").hide();
+
 $("#cli").on("click",function (){
 	var tex = $('#opt').val();
 	var pot = $('#pot').val();
 	location.href="javafood?javafood=m&opt="+tex+"&pot="+pot
 	})
+		
+$('#spa').on('mouseover',function(){
+	$('#di').slideDown();
+})
+
+$('#di').on('mouseleave',function(){
+	$('#di').slideUp();
+})
